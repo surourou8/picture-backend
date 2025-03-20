365db1452353ccf59d95e8cde5b979ae8a2198cc
surourou8 <surourou2019@email.szu.edu.cn>
2025-03-20 22:46:44 +0800

    图片模块： 1.腾讯云后端操作对象存储 2.FileController：通用文件上传接口（通用） 3.FileManager（图片上传） 4.PictureController：图片管理（增删改查，分页查询）

---
+++ src/main/java/com/srr/picturebackend/mapper/PictureMapper.java
@@ -0,0 +1,14 @@
+package com.srr.picturebackend.mapper;
+
+import com.srr.picturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author srr
+ * @description 针对表【picture(图片)】的数据库操作Mapper
+ * @createDate
+ * @Entity com.srr.picturebackend.model.entity.Picture
+ */
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
---
+++ src/main/java/com/srr/picturebackend/service/PictureService.java
@@ -0,0 +1,66 @@
+package com.srr.picturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.srr.picturebackend.model.dto.picture.PictureQueryRequest;
+import com.srr.picturebackend.model.dto.picture.PictureUploadRequest;
+import com.srr.picturebackend.model.entity.Picture;
+import com.srr.picturebackend.model.entity.User;
+import com.srr.picturebackend.model.vo.PictureVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author srr
+ * @description 针对表【picture(图片)】的数据库操作Service
+ * @createDate
+ */
+public interface PictureService extends IService<Picture> {
+
+    /**
+     * 校验图片
+     *
+     * @param picture
+     */
+    void validPicture(Picture picture);
+
+    /**
+     * 上传图片
+     *
+     * @param multipartFile
+     * @param pictureUploadRequest
+     * @param loginUser
+     * @return
+     */
+    PictureVO uploadPicture(MultipartFile multipartFile,
+                            PictureUploadRequest pictureUploadRequest,
+                            User loginUser);
+
+    /**
+     * 获取图片包装类（单条）
+     *
+     * @param picture
+     * @param request
+     * @return
+     */
+    PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    /**
+     * 获取图片包装类（分页）
+     *
+     * @param picturePage
+     * @param request
+     * @return
+     */
+    Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    /**
+     * 获取查询对象
+     *
+     * @param pictureQueryRequest
+     * @return
+     */
+    QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+}
